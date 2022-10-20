8020d3ecf16a2b3da370a503ffa6f9b041ca35a1
h110401 <hung110401@gmail.com>
2022-10-20 17:16:01 +0700

    bt1

---
+++ src/main/java/rikkei/academy/repository/blog/IBlogRepository.java
@@ -0,0 +1,7 @@
+package rikkei.academy.repository.blog;
+
+import rikkei.academy.model.Blog;
+import rikkei.academy.repository.IGeneralRepository;
+
+public interface IBlogRepository extends IGeneralRepository<Blog> {
+}
